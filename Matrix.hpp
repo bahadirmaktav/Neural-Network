4568f10757bb581f6c75fe6e2cea3bb4b2a9e6bf
Mehmet Bahadır Maktav <bahadirmaktavv@gmail.com>
2022-12-26 19:44:26 +0300

    Multiply/map functionalities added to the Matrix class.

--- Matrix.hpp
+++ Matrix.hpp
@@ -2,6 +2,7 @@
 #define MATRIX_HPP_
 
 #include <iostream>
+#include <functional>
 
 class Matrix {
 public:
@@ -25,12 +26,14 @@ public:
     }
     void PrintMatrix() {
         for(int i = 0; i < rowNum; i++) {
-            std::cout << "\n";
             for(int j = 0; j < colNum; j++) {
                 std::cout << data[i][j] << " ";
             }
+            std::cout << "\n";
         }
     }
+
+    // Operator overloading functions returns new Matrix except '='.
     void operator = (const Matrix & iMatrix) {
         if(iMatrix.rowNum != rowNum || iMatrix.colNum != colNum) {
             throw std::runtime_error("Error : You cannot equalize matrices with different dimensions!");
@@ -64,6 +67,38 @@ public:
             }
         }
         return *res;
+    }
+    Matrix & operator * (const Matrix & iMatrix) {
+        if(colNum != iMatrix.rowNum) {
+            throw std::runtime_error("Error : First matrix column number must equal to second matrix row number to multiply these matrices!");
+        }
+        Matrix * res = new Matrix(rowNum, iMatrix.colNum);
+        for(int i = 0; i < rowNum; i++) {
+            for(int j = 0; j < iMatrix.colNum; j++) {
+                for(int c = 0; c < iMatrix.rowNum; c++) {
+                    res->data[i][j] += (data[i][c] * iMatrix.data[c][j]); 
+                }
+            }
+        }
+        return *res;
+    }
+    Matrix & operator * (const double & iConst) {
+        Matrix * res = new Matrix(rowNum, colNum);
+        for(int i = 0; i < rowNum; i++) {
+            for(int j = 0; j < colNum; j++) {
+                res->data[i][j] = data[i][j] * iConst;
+            }
+        }
+        return *res;
+    }
+
+    // Below functions changes the current Matrix object.
+    void Map(std::function<double(double)> handlerFn) {
+        for(int i = 0; i < rowNum; i++) {
+            for(int j = 0; j < colNum; j++) {
+                data[i][j] = handlerFn(data[i][j]);
+            }
+        }
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -3,14 +3,36 @@
 
 int main(int, char**) {
     try {
-        Matrix m1(4, 6);
-        Matrix m2(4, 6);
-        Matrix m3(4, 6);
-        m1.data[0][0] = 1; m1.data[0][1] = 2; m1.data[3][2] = 3; m1.data[0][3] = 4; m1.data[0][4] = 5;
-        m2.data[0][0] = 5; m2.data[0][1] = 2; m2.data[3][2] = 9; m2.data[0][3] = 4; m2.data[0][4] = 5;
-        m3 = m1 + m2;
-        m3 = m1 - m2;
+        Matrix m1(2, 3);
+        Matrix m2(3, 4);
+        Matrix m3(2, 4);
+
+        m1.data[0][0] = 8; m1.data[0][1] = -1; m1.data[0][2] = 4;
+        m1.data[1][0] = -5; m1.data[1][1] = 3; m1.data[1][2] = 6;
+        std::cout << "Matrix m1 : " << std::endl;
+        m1.PrintMatrix();
+
+        m2.data[0][0] = 7; m2.data[0][1] = -4; m2.data[0][2] = 8; m2.data[0][3] = 7;
+        m2.data[1][0] = -1; m2.data[1][1] = 7; m2.data[1][2] = -3; m2.data[1][3] = 3;
+        m2.data[2][0] = 3; m2.data[2][1] = 2; m2.data[2][2] = 4; m2.data[2][3] = 2;
+        std::cout << "Matrix m2 : " << std::endl;
+        m2.PrintMatrix();
+
+        std::cout << "Matrix m3 = m1 * m2 : " << std::endl;
+        m3 = m1 * m2;
         m3.PrintMatrix();
+
+        Matrix m4(2, 4);
+        m4 = m3 * 5;
+        std::cout << "Matrix m4 = m3 * 5 : " << std::endl;
+        m4.PrintMatrix();
+
+        std::function<double(double)> halfFn = [](double input) -> double {
+            return input / 2;
+        };
+        m4.Map(halfFn);
+        std::cout << "Matrix m4 = all elements divided by 2 : " << std::endl;
+        m4.PrintMatrix();
     }
     catch(const std::exception& e) {
         std::cerr << e.what() << '\n';
